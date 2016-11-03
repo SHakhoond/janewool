e4c8c28cf46758e9c880eca19269aefaec085839
shakhoond <shayan.akhoond@gmail.com>
2016-11-03 14:18:28 +1300

    finishing shopping cart

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(session({
   secret: 'mysupersecret', 
   resave: false, 
   saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-  cookie: { maxAge:180 * 60 *1000 }
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 *1000 }
 }));
 app.use(flash());
 app.use(passport.initialize());
